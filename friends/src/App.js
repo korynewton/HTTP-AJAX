1c29ef3cf3393e70b798ae45c509120f79564ba5
korynewton <korynewtondev@gmail.com>
2019-02-27 11:37:08 -0800

    axios get request working properly

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+import FriendsList from './components/FriendsLists'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list : []
+    }
+  }
+
+componentDidMount() {
+  axios.get("http://localhost:5000/friends") 
+  .then( (res) => {
+    console.log(res)
+  } )
+  .catch( (err) => {
+    console.log(err)
+  } )
+}
+
   render() {
     return (
       <div>
-        <h1>Testing</h1>
+        <FriendsList />
       </div>
     );
   }
---
+++ friends/src/components/FriendsLists.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FriendsList = (props) => {
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  )
+}
+
+
+export default FriendsList
